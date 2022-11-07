ddf72fa66a4b718ab549597ca988f9c6df064082
Aslanov Nijad <aslanlinicad@gmail.com>
2022-11-06 16:47:30 -0800

    orm

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -4,10 +4,18 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface UserService extends CrudService<UserDTO, String> {
+public interface UserService {
 
-    List<UserDTO> findManagers();
+    List<UserDTO> listAllUsers();
 
-    List<UserDTO> findEmployees();
+    UserDTO findByUserName(String username);
+
+    void save(UserDTO dto);
+
+    UserDTO update(UserDTO dto);
+
+    void deleteByUserName(String username);
+
 
 }
+
