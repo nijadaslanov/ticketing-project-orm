18d02e4289c3e4c6d32d8e536a2f7e5dfba8956a
Aslanov Nijad <aslanlinicad@gmail.com>
2022-11-08 19:00:48 -0800

    orm

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -26,6 +23,7 @@ public class BaseEntity {
 
     private Boolean isDeleted = false;
 
+    @PrePersist
     public void onPrePersist() {
         this.insertDateTime = LocalDateTime.now();
         this.lastUpdateDateTime = LocalDateTime.now();
