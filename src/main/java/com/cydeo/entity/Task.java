a6114e71342eabf4a403eeb3369c3562791643e0
Aslanov Nijad <aslanlinicad@gmail.com>
2022-11-09 18:27:10 -0800

    task entity

--- src/main/java/com/cydeo/entity/Task.java
+++ src/main/java/com/cydeo/entity/Task.java
@@ -56,4 +56,8 @@ public class Task extends BaseEntity {
 
 
 
+
+
+
+
 }
---
+++ src/main/java/com/cydeo/repository/TaskRepository.java
@@ -0,0 +1,26 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
